e04478f2464fe802f8f37972ff153fa7960c5706
hermann <hermann.samimi@yahoo.com>
2025-06-21 15:20:55 +0300

    requriements.txt file added to repo for render deployment

--- pipeline.py
+++ pipeline.py
@@ -7,57 +7,60 @@ dotenv.load_dotenv()
 from time import sleep
 
 
-# ------------------------ # Example of using an API to get movie data ------------------------
+# ------------------------------------------ get data from an API ----------------
+# RAPID API LINK : https://rapidapi.com/principalapis/api/currency-conversion-and-exchange-rates/playground/endpoint_01c2f371-2ab0-4e56-98f4-e4f4149e9cfc
+import requests
 
 
-# url = "https://movie-database-api1.p.rapidapi.com/list_movies.json"
+# start_date = 
+# end_date = 
+# base = "USD"
+# symbols = "EUR,GBP"
 
-# querystring = {"limit":"20","page":"1","quality":"all","genre":"all","minimum_rating":"0","query_term":"0","sort_by":"date_added","order_by":"desc","with_rt_ratings":"false"}
+url = "https://currency-conversion-and-exchange-rates.p.rapidapi.com/timeseries"
 
-# headers = {
-#     "x-rapidapi-key": "11c830c25emshf8289b2a0f0cecep1a192fjsna96600ee8ec1",
-#     "x-rapidapi-host": "movie-database-api1.p.rapidapi.com"
-# }
+querystring = {"start_date":"2025-01-01","end_date":"2019-01-30","base":"USD","symbols":"EUR,GBP"}
 
-# response = requests.get(url, headers=headers, params=querystring)
+headers = {
+	"x-rapidapi-key": "11c830c25emshf8289b2a0f0cecep1a192fjsna96600ee8ec1",
+	"x-rapidapi-host": "currency-conversion-and-exchange-rates.p.rapidapi.com"
+}
 
-# data = response.json()
-# data = json.dumps(data, indent=4)
+response = requests.get(url, headers=headers, params=querystring)
 
-# print(data)
+output = response.json()
+print(json.dumps(output, indent=4))
 
 
 
-# ------------------------ # Example of read data from a txt file  ------------------------
-# read data from a file
-with open("/Users/hermann/Documents/Python Course/data/test_persian.txt", "r") as file:
-    data = file.read()
+# # ------------------------ # Example of read data from a txt file  ------------------------
+# # read data from a file
+# with open("/Users/hermann/Documents/Python Course/data/test_persian.txt", "r") as file:
+#     data = file.read()
 
 
 
 # ------------------------ # create a telegram bot -------------------------
 
 # Replace with your values
-    BOT_TOKEN = os.getenv("telegram_token")
-    CHAT_ID = "-1002830405443"
+    # BOT_TOKEN = os.getenv("telegram_token")
+    # CHAT_ID = "-1002830405443"
 
-    MESSAGE = data
-
-
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": MESSAGE
-    }
-
-    response = requests.post(url, data=payload)
-
-    if response.status_code == 200:
-        print("Message sent successfully.")
-    else:
-        print("Failed to send message:", response.text)
-    sleep(1)  
+    # MESSAGE = data
 
 
+    # url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    # payload = {
+    #     "chat_id": CHAT_ID,
+    #     "text": MESSAGE
+    # }
+
+    # response = requests.post(url, data=payload)
+
+    # if response.status_code == 200:
+    #     print("Message sent successfully.")
+    # else:
+    #     print("Failed to send message:", response.text)
+    # sleep(1)  
 
